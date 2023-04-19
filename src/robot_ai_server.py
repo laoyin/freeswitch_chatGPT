a76061182156e492a685a1b5b916a6f7db1477b1
尹行攀 <yinxingpan@aegis-data.cn>
2023-04-19 14:27:34 +0800

    update html server

--- src/robot_ai_server.py
+++ src/robot_ai_server.py
@@ -1,4 +1,15 @@
+import os
 from open_ai.chat_ai import ws_app
+from sanic.response import html
+
+
+
+
+
+@ws_app.route('/')
+async def index(request):
+    content = open(os.path.join("open_ai", "chat_ai.html"), "r").read()
+    return html(content)
 
 
 if __name__ == "__main__":
